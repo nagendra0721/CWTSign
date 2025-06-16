20045d37a8095392a453e729b0ae51f43a71dd63
nagendra0721 <nagendra0718@gmail.com>
2025-06-16 22:02:36 +0530

    enforcing the Notnull to dto
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- CWTSign/src/main/java/io/mosip/cwtsign/dto/CWTVerifyRequestDto.java
+++ CWTSign/src/main/java/io/mosip/cwtsign/dto/CWTVerifyRequestDto.java
@@ -1,5 +1,6 @@
 package io.mosip.cwtsign.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CWTVerifyRequestDto {
-    
+    @NotNull
     private String cwtSignedData;
 }
