a55579f1e95df729ab72c1f6e9ede9490bf42ef9
nagendra0721 <nagendra0718@gmail.com>
2025-06-16 17:19:33 +0530

    Add files via upload

---
+++ CWTSign/CWTSignApplication.java
@@ -0,0 +1,20 @@
+package io.mosip.cwtsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CWTSignApplication {
+
+	/**
+	 * The main method to start the CWT Sign application.
+	 *
+	 * @param args command line arguments
+	 */
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CWTSignApplication.class, args);
+	}
+
+}
